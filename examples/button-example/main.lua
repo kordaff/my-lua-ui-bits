7ea540ca23d6962218adecdaa46eead947e58551
Phillip Hartfield Sr <phil681@yahoo.com>
2025-04-02 11:40:27 -0500

    Update main.lua
    fixed

--- examples/button-example/main.lua
+++ examples/button-example/main.lua
@@ -1,12 +1,14 @@
-require "entry"   require "button"
+require "button"
+
 function love.load()
   font = love.graphics.newFont("Montserrat-Regular.ttf", 36)
-  b2 = Button:new( {x=15,y=250,name="---",font=font,visible=true } )
-  e = Entry:new({font=font,text="",x=15,y=80,xpad=10,ypad=10,visible=true,focus=true})
-  e.fn=function() b2.name=e.text e.text="" end
+  b2 = Button:new( {x=250,y=50,name="this is a button",font=font,visible=true } )
 end
-function love.textinput(t) e.text=e.text .. t end
-function love.draw() b2:draw() e:draw() end
+
+function love.draw() 
+  b2:draw() 
+end
+
 function love.keypressed(key, scancode, isrepeat)
   if key == "escape" then love.event.quit() end
   e:keypressed(key)
